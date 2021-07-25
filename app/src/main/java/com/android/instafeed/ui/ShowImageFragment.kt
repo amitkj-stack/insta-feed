f9c3e3395c8b4bf4340b215a68fbc953dbc777f6
amitkumarjaiswal-novopay <amitkumarjaiswal-novopay@novopay.in>
2021-07-25 19:23:53 +0530

    Implemented new fragment to show image

---
+++ app/src/main/java/com/android/instafeed/ui/ShowImageFragment.kt
@@ -0,0 +1,78 @@
+package com.android.instafeed.ui
+
+import android.content.res.Configuration
+import android.graphics.drawable.Drawable
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.DialogFragment
+import com.android.instafeed.R
+import com.android.instafeed.databinding.FragmentShowImageBinding
+import com.android.instafeed.support.AppConstants
+import com.android.instafeed.support.Utils.setHeightPercent
+import com.android.instafeed.support.Utils.setWidthPercent
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.Target
+
+class ShowImageFragment : DialogFragment() {
+    private lateinit var binding: FragmentShowImageBinding
+    private lateinit var url: String
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        retainInstance = true
+        url = arguments?.getString(AppConstants.PARAM.EXTRA_URL).toString()
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_show_image, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        if (resources.configuration.orientation == Configuration.ORIENTATION_PORTRAIT) {
+            setWidthPercent(90)
+        } else {
+            setHeightPercent(90)
+        }
+        Glide.with(this)
+            .load(url)
+            .placeholder(R.drawable.ic_launcher_foreground)
+            .listener(object : RequestListener<Drawable?> {
+                override fun onLoadFailed(
+                    e: GlideException?,
+                    model: Any,
+                    target: Target<Drawable?>,
+                    isFirstResource: Boolean
+                ): Boolean {
+                    binding.progressBar.visibility = View.GONE
+                    return false // important to return false so the error placeholder can be placed
+                }
+
+                override fun onResourceReady(
+                    resource: Drawable?,
+                    model: Any,
+                    target: Target<Drawable?>,
+                    dataSource: DataSource,
+                    isFirstResource: Boolean
+                ): Boolean {
+                    binding.progressBar.visibility = View.GONE
+                    return false
+                }
+            })
+            .into(binding.ivPic)
+    }
+
+
+}
